4d38633b559d259eb509dc39f51c34644b3b90f4
Andreas <andrew.eracleous@gmail.com>
2014-11-26 22:24:38 +0200

    Updated CSS style and Javascript file

--- js/main.js
+++ js/main.js
@@ -1,30 +1,6 @@
-$(document).ready(function(){
-  function whichAnimationEvent(){
-    var t,
-        el = document.createElement("fakeelement");
-
-    var animations = {
-      "animation"      : "animationend",
-      "OAnimation"     : "oAnimationEnd",
-      "MozAnimation"   : "animationend",
-      "WebkitAnimation": "webkitAnimationEnd"
-    }
-
-    for (t in animations){
-      if (el.style[t] !== undefined){
-        return animations[t];
-      }
-    }
-  }
-
-  var animationEvent = whichAnimationEvent();
-
-  $("#btn-close").click(function(){
-    $("#notification-bar").addClass("animate");
-    $("#notification-bar").one(animationEvent,
-                function(event) {
-      // Do something when the animation ends
-      $(this).addClass("hide");
-    });
-  });
+$("input[type=checkbox]").click(function(){
+  var $this = $(this);
+  setTimeout(function(){
+    $this.siblings("#notification-bar").toggleClass("hide", "block");
+  }, 500, $this);
 });
